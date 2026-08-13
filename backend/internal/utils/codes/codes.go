c1f799de50a74c0dc23fe26f06793f48c61ffce7
impl0x <implox.v1@gmail.com>
2026-08-14 01:04:06 +0530

    enforced NounAdjective naming scheme for codes

--- backend/internal/utils/codes/codes.go
+++ backend/internal/utils/codes/codes.go
@@ -1,10 +1,13 @@
 package codes
 
+// A short string sequence with underscores and all caps signifying in short what went wrong or right
 type Code string
 
-// The error codes that are returned in the json
-//
 // Helpful to the frontend application to know these codes
+
+// NounAdjective format
+
+// The error codes that are returned in the json
 const (
 	Unknown Code = "UNKNOWN"
 
@@ -12,29 +15,35 @@ const (
 	MethodNotAllowed    Code = "METHOD_NOT_ALLOWED"
 	InternalServerError Code = "INTERNAL_SERVER_ERROR"
 
-	InvalidJSON Code = "INVALID_JSON"
+	JSONInvalid Code = "JSON_INVALID"
 	EOF         Code = "EOF"
 
 	ValidationError Code = "VALIDATION_ERROR"
 
-	MissingIdentifier  Code = "MISSING_IDENTIFIER"
-	InvalidCredentials Code = "INVALID_CREDENTIALS"
-	UserBanned         Code = "USER_BANNED"
-	UserUnverified     Code = "USER_UNVERIFIED"
+	IdentifierMissing  Code = "IDENTIFIER_MISSING"
+	CredentialsInvalid Code = "CREDENTIALS_INVALID"
 
+	UserBanned       Code = "USER_BANNED"
+	UserUnverified   Code = "USER_UNVERIFIED"
 	UserExists       Code = "USER_ALREADY_EXISTS"
 	UserNotOldEnough Code = "USER_NOT_OLD_ENOUGH"
 	UserTooOld       Code = "USER_TOO_OLD"
+
 	UsernameAlreadyExists Code = "USERNAME_ALREADY_EXISTS"
 
-	ReferenceIDNotFound Code = "REFERENCE_ID_NOT_FOUND"
-	OTPExpired Code = "OTP_EXPIRED"
+	OTPExpired   Code = "OTP_EXPIRED"
 	OTPIncorrect Code = "OTP_INCORRECT"
+
+	AccessTokenInvalid  Code = "ACCESS_TOKEN_INVALID"
+	AccessTokenExpired  Code = "ACCESS_TOKEN_EXPIRED"
+	RefreshTokenInvalid Code = "REFRESH_TOKEN_INVALID"
+	RefreshTokenExpired Code = "REFRESH_TOKEN_EXPIRED"
 )
 
 // the success codes returned in the json
 const (
-	LoginSuccess  Code = "LOGIN_SUCCESS"
-	TwoFARequired Code = "TWO_FACTOR_REQUIRED"
+	LoginSuccess    Code = "LOGIN_SUCCESS"
+	TwoFARequired   Code = "TWO_FACTOR_REQUIRED"
 	RegisterSuccess Code = "REGISTER_SUCCESS"
+	RefreshSuccess  Code = "REFRESH_SUCCESS"
 )
