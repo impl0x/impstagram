eafc1c32440f31a7a17ad1cac65a66bd718b85fd
impl0x <implox.v1@gmail.com>
2026-08-18 03:28:05 +0530

    db migration changes: strings be "" instead of null due to pgx nuances

--- backend/migrations/20260817141619_init_schema.sql
+++ backend/migrations/20260817141619_init_schema.sql
@@ -6,15 +6,15 @@ CREATE TYPE auth_channel AS ENUM ('email','phone','username','totp');
 CREATE TABLE users (
     id UUID PRIMARY KEY DEFAULT gen_random_uuid(),
     username TEXT UNIQUE NOT NULL,
-    email TEXT UNIQUE,
-    phone TEXT UNIQUE,
-    totp_secret_key TEXT UNIQUE,
+    email TEXT UNIQUE NOT NULL DEFAULT '',
+    phone TEXT UNIQUE NOT NULL DEFAULT '',
+    totp_secret_key TEXT UNIQUE NOT NULL DEFAULT '',
     password_hash TEXT NOT NULL,
     dob DATE NOT NULL,
     status account_status NOT NULL DEFAULT 'unverified',
     two_fas auth_channel[], -- can be null, if so then 2fa is disabled 
-    created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
 CREATE TABLE user_sessions (
