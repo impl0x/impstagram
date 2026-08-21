3c941f57712982f7293a924195c5eb9cc73f8561
impl0x <implox.v1@gmail.com>
2026-08-22 00:11:09 +0530

    updated middleware to use apperr instead of defer ing a response

--- backend/internal/features/auth/middleware.go
+++ backend/internal/features/auth/middleware.go
@@ -1,10 +1,10 @@
 package auth
 
 import (
+	"backend/internal/pkg/apperr"
 	"backend/internal/pkg/jwt"
 	"backend/internal/pkg/response"
 	"backend/internal/pkg/ttlcache"
-	"net/http"
 	"time"
 
 	"github.com/google/uuid"
@@ -28,28 +28,10 @@ var jwtTokenBlockList = ttlcache.New[uuid.UUID, struct{}](ruleTTLCacheCleanInter
 // Any handler wrapped with this middleware can safely assure that mo.Context["jwt"] will ALWAYS return a valid [jwt.AccessTokenPayload] struct
 func Middleware(next mo.HandlerFunc) mo.HandlerFunc {
 	return func(c *mo.Context) (handlerErr error) {
-		// We defer a function which returns a unauthorized error with a errorMessage message variable if the variable has been set.
-		// this is done to reduce redundancy of the same type of code
-		var statusCode int = http.StatusUnauthorized
-		var errorMessage string
-
-		defer func() {
-			if errorMessage != "" {
-				handlerErr = c.JSON(
-					statusCode,
-					response.Error(
-						response.CodeUnauthorized,
-						errorMessage,
-					),
-				)
-			}
-		}()
-
 		// get the auth header value from the request header
 		authHeader := c.Request().Header.Get("authorization")
 		if authHeader == "" {
-			errorMessage = "Authorization header missing or empty"
-			return
+			return apperr.NewUnauthorized(response.CodeUnauthorized, "Authorization header missing or empty")
 		}
 
 		// Decode the token into a jwt payload struct
@@ -58,39 +40,35 @@ func Middleware(next mo.HandlerFunc) mo.HandlerFunc {
 
 		// Check if jwt decode fails or if the signature is incorrect
 		if err != nil {
-			switch err {
+			switch err { // assuming jwt.VerifyToken can only return these 2 errors
 			case jwt.ErrInvalidJWTToken:
-				errorMessage = "Invalid authorization token"
+				return apperr.NewUnauthorized(response.CodeUnauthorized, "Invalid authorization token")
 			case jwt.ErrIncorrectJWTToken:
-				errorMessage = "Authorization token has been tampered with, signature mismatch"
+				return apperr.NewUnauthorized(response.CodeUnauthorized, "Authorization token has been tampered with, signature mismatch")
 			}
-			return
 		}
 
 		// validating the jwt payload (optional)
 		errs := validator.Validate(accessTokenPayload) //! we do not technically need to validate a access token if the server is correctly issuing tokens, comment this part out if everything is tested and working
 		if errs != nil {
-			errorMessage = "Invalid authorization token issued by the server! Validation error for the JSON payload"
-			return
+			return apperr.NewUnauthorized(response.CodeUnauthorized, "Invalid authorization token issued by the server! Validation error for the JSON payload")
 		}
 
 		// Converting the json struct into a usable data type for our app
 		accessTokenJwt, err := accessTokenPayload.Convert()
 		if err != nil {
-			errorMessage = "Invalid data in the JSON payload for authorization token"
-			return
+			return apperr.NewUnauthorized(response.CodeUnauthorized, "Invalid data in the JSON payload for authorization token")
 		}
 		// Checking if the jwt has expired
 		if accessTokenJwt.ExpiresAt.Before(time.Now()) {
-			errorMessage = "Token has expired, please refresh it using the refresh token"
-			return
+			return apperr.NewUnauthorized(response.CodeUnauthorized, "Token has expired, please refresh it using the refresh token")
 		}
 
 		// Checking if the jwt is in jwt token block list
 		_, _, ok := jwtTokenBlockList.Get(accessTokenJwt.JwtID)
 		if ok {
-			errorMessage = "Unauthorized" // try not to give the client much info about *why* it is unauthorized, even though we know that this jwt is blacklisted
-			return
+			// try not to give the client much info about *why* it is unauthorized, even though we know that this jwt is blacklisted
+			return apperr.NewUnauthorized(response.CodeUnauthorized, "Invalid authorization token")
 		}
 
 		// We store the access token data struct in the context's store map
